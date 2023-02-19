28da48038d3983723557fa3e1d91bfeff8de4a74
David Aremu <66888450+kingola1@users.noreply.github.com>
2023-02-19 08:46:06 +0000

    fixed vics branch to merge

--- App.js
+++ App.js
@@ -1,42 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native';
-import Logo from "./Pages/Signup/Logo";
-import Input from "./Pages/Signup/Input";
-import ResetInput from './Pages/Signup/ResetInput';
-import ChatScreen from './Pages/Dmbuyer/ChatScreen';
-import ProductPage from './Pages/Product/ProductPage';
-import OrderModal from './Pages/Product/OrderModal';
-import ViewDeal from './Pages/Transaction/ViewDeal';
-import ViewModal from './Pages/Transaction/ViewModal';
+import { useFonts } from 'expo-font';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
+import React from 'react'
 
+import {Login, DashboardScreen, MyNetworkScreen, UserProfileScreen, SendFunds } from './screens';
+
+// import Logo from "./Pages/Signup/Logo";
+// import Input from "./Pages/Signup/Input";
+// import ResetInput from './Pages/Signup/ResetInput';
+// import ChatScreen from './Pages/Dmbuyer/ChatScreen';
+// import ProductPage from './Pages/Product/ProductPage';
+// import OrderModal from './Pages/Product/OrderModal';
+// import ViewDeal from './Pages/Transaction/ViewDeal';
+// import ViewModal from './Pages/Transaction/ViewModal';
 
 
 export default function App() {
+
+
+  const [fontsLoaded] = useFonts({
+    Cabin : require('./assets/fonts/Cabin-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) return null;
+  
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-       {/* <Logo /> */}
-      {/* <Input/> */}
-      {/* <ResetInput /> */}
-      {/* <ChatScreen /> */}
-      {/* <ProductPage /> */}
-      {/* <OrderModal /> */}
-      {/* <ViewDeal /> */}
-      <ViewModal />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown: false}}> 
 
-    </View>
-  );
+        <Stack.Screen name="Login" component={Login} />
+        
+        <Stack.Screen name="DashboardScreen" component={DashboardScreen} />
+        <Stack.Screen name="MyNetworkScreen" component={MyNetworkScreen} />
+        <Stack.Screen name="UserProfileScreen" component={UserProfileScreen} />
+        <Stack.Screen name="SendFunds" component={SendFunds} />
+
+       </Stack.Navigator >
+    </NavigationContainer>
+
+    
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: '#EB6117',
-    backgroundColor: '#ffffff',
-    // forDmscreen
-    // backgroundColor: '#182436',
-    // borderColor: '#9B9B9B',
-    // borderWidth:3,
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-});
