60c4ec5714745b6f3b74c0022cfbe2bfb3eb5bed
David Aremu <segun8427@gmail.com>
2023-04-14 09:41:45 +0100

    fixed babel.js file

--- babel.config.js
+++ babel.config.js
@@ -4,8 +4,6 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       ["module:react-native-dotenv"],
-
-      'react-native-reanimated/plugin',
     ],
   };
 };
